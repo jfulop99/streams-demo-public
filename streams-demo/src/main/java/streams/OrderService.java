fc152c8069d2ffa49b0845e3ba06c88c818d6362
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-06-12 08:03:16 +0200

    Stream gyakorlat

--- streams-demo/src/main/java/streams/OrderService.java
+++ streams-demo/src/main/java/streams/OrderService.java
@@ -13,4 +13,38 @@ public class OrderService {
         orders.add(order);
     }
 
+    public long countOrderByStatus(String status) {
+        return orders.stream().filter(order -> order.getStatus().equals(status)).count();
+    }
+
+    public List<Order> collectOrdersWithProductCategory(String category) {
+        return orders.stream()
+                .filter(order -> order.getProducts()
+                        .stream()
+                        .anyMatch(p -> p.getCategory().equals(category)))
+                .collect(Collectors.toList());
+    }
+
+    public List<Product> productsOverAmountPrice(int amount) {
+
+        return orders.stream()
+                .flatMap(order -> order.getProducts().stream())
+                .filter(product -> product.getPrice() > amount)
+                .collect(Collectors.toList());
+    }
+
+    public double amountBetweenTwoDates(LocalDate startDate, LocalDate endDate) {
+        return orders.stream()
+                .filter(order -> order.getOrderDate().isAfter(startDate) && order.getOrderDate().isBefore(endDate))
+                .flatMap(order -> order.getProducts().stream())
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
+
+    public Order orderWithMostExpensiveProduct() {
+        return orders.stream()
+                .max(Comparator.comparing(order -> order.getProducts().stream().map(Product::getPrice).max(Comparator.comparing(Double::doubleValue)).orElseThrow()))
+                .orElseThrow();
+
+    }
 }
--- streams-demo/src/test/java/streams/OrderServiceTest.java
+++ streams-demo/src/test/java/streams/OrderServiceTest.java
@@ -1,8 +1,10 @@
 package streams;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,7 +25,7 @@ class OrderServiceTest {
 
         Order o1 = new Order("pending", LocalDate.of(2021,06,07));
         o1.addProduct(p1);
-        o1.addProduct(p2);
+        o1.addProduct(p4);
         o1.addProduct(p5);
 
         Order o2 = new Order("on delivery", LocalDate.of(2021,06,01));
@@ -37,4 +39,35 @@ class OrderServiceTest {
     }
 
 
+    @Test
+    void countOrderByStatusTest() {
+
+        long result = ordersService.countOrderByStatus("pending");
+
+        assertEquals(1, result);
+
+    }
+
+    @Test
+    void collectOrdersWithProductCategoryTest() {
+        assertEquals( 1, ordersService.collectOrdersWithProductCategory("Book").size() );
+    }
+
+    @Test
+    void productsOverAmountPrice() {
+        assertEquals(3, ordersService.productsOverAmountPrice(1999).size());
+        assertEquals(1, ordersService.productsOverAmountPrice(2399).size());
+    }
+
+    @Test
+    void amountBetweenTwoDates() {
+        assertEquals(6940.0, ordersService.amountBetweenTwoDates(LocalDate.of(2021, 5,31), LocalDate.now()));
+        assertEquals(2140.0, ordersService.amountBetweenTwoDates(LocalDate.of(2021, 6,1), LocalDate.now()));
+    }
+
+    @Test
+    void orderWithMostExpensiveProduct() {
+        Order order = ordersService.orderWithMostExpensiveProduct();
+        assertEquals("on delivery", order.getStatus());
+    }
 }
